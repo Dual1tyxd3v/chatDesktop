6fef4d3fbda5b6cc237629def3b8226a6d89a5e1
B3rzxd3v <d3vilc0re@gmail.com>
2023-05-12 01:34:47 +0700

    Add Select & Server Menu logic

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,28 @@
-const images = document.querySelectorAll('img');
-const inp = document.querySelector('input[type="file"]');
-images.forEach(img => {
-  img.style.width = '300px';
+const selectHeader = document.querySelector('.header__title');
+const selectOptions = document.querySelectorAll('.header__option');
+const headerText = document.querySelector('.header__text');
+const serversItems = document.querySelectorAll('.servers__item:not(.servers__item--btnAdd)');
+
+// --SERVERS ITEMS SWITCH
+serversItems.forEach(server => server.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  document.querySelector('.servers__item--active').classList.remove('servers__item--active');
+  e.target.classList.add('servers__item--active');
+}));
+//
+
+// --SELECT LOGIC
+selectHeader.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  selectHeader.classList.toggle('header__title--opened');
 });
 
-
-inp.addEventListener('change', (e) => {
-  const x = e.target.files[0];
-})
+selectOptions.forEach(option => option.addEventListener('click', (e) => {
+  headerText.textContent = e.target.textContent;
+  document.querySelector('.header__option--active').classList.remove('header__option--active');
+  e.target.classList.add('header__option--active');
+  selectHeader.classList.toggle('header__title--opened');
+}));
+//
