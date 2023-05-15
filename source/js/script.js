8169c78f36cde28b5aece2308e29323deb7ba723
B3rzxd3v <d3vilc0re@gmail.com>
2023-05-15 11:45:55 +0700

    Add slider logic

--- source/js/script.js
+++ source/js/script.js
@@ -5,8 +5,14 @@ const serversItems = document.querySelectorAll('.servers__item:not(.servers__ite
 const groupItems = document.querySelectorAll('.group__item');
 const headTitle = document.querySelector('.head__title');
 const messageInput = document.querySelector('.new-message__input');
+const slider = document.querySelector('.slider');
+const nextBtn = document.querySelector('.info__slideBtn--next');
+const prevBtn = document.querySelector('.info__slideBtn--prev');
+const slides = document.querySelectorAll('.info__item');
 
 const INPUT_PLACEHOLDER = 'Message in';
+const SLIDER_OFFSET = 200;
+const MAX_SLIDES = slides.length;
 
 function switchActiveClass(e, className) {
   e.preventDefault();
@@ -29,7 +35,7 @@ serversItems.forEach(server => server.addEventListener('click', (e) => switchAct
 groupItems.forEach(groupItem => groupItem.addEventListener('click', (e) => switchActiveClass(e, 'group__item--active')));
 //
 
-// --SELECT LOGIC
+// --SELECT MENU
 selectHeader.addEventListener('click', (e) => {
   e.preventDefault();
 
@@ -42,4 +48,25 @@ selectOptions.forEach(option => option.addEventListener('click', (e) => {
   e.target.classList.add('header__option--active');
   selectHeader.classList.toggle('header__title--opened');
 }));
+//
+
+// SLIDER
+let slideIndex = 0;
+function switchSlide(direction) {
+  slideIndex = slideIndex + direction;
+  if (slideIndex === 0) {
+    !prevBtn.classList.contains('info__slideBtn--hidden') && prevBtn.classList.add('info__slideBtn--hidden');
+  } else {
+    prevBtn.classList.remove('info__slideBtn--hidden');
+  }
+  if (slideIndex === (MAX_SLIDES - 1)) {
+    !nextBtn.classList.contains('info__slideBtn--hidden') && nextBtn.classList.add('info__slideBtn--hidden');
+  } else {
+    nextBtn.classList.remove('info__slideBtn--hidden');
+  }
+  slider.style.transform = `translateX(-${SLIDER_OFFSET * slideIndex}px)`;
+}
+
+nextBtn.addEventListener('click', () => switchSlide(1));
+prevBtn.addEventListener('click', () => switchSlide(-1));
 //
